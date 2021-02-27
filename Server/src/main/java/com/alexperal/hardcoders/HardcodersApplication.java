0e012d61c935da74e77e298b1d9b70f0945b028f
Alvaro Furlan Falcao <contact@alvarofurlanfalcao.me>
2021-02-27 12:09:58 +0000

    Initial Spring server setup

---
+++ Server/src/main/java/com/alexperal/hardcoders/HardcodersApplication.java
@@ -0,0 +1,13 @@
+package com.alexperal.hardcoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardcodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardcodersApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/alexperal/hardcoders/HardcodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexperal.hardcoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardcodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
